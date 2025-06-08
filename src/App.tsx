edba7fc340db6e02201a96017c40947ba6fe1517
Varun-Kelkar <vnkelkar11@gmail.com>
2025-06-08 12:47:11 +0530

    added description & a few more user names

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>LRU Cache Implementation</h1>
+      <h1>LRU Cache Demo</h1>
+      <p className="description">
+        This is a simple demo of an LRU Cache used in React. Type a name in the
+        search box and press enter to see the results. The cache will store the
+        last 4 searches, which you can click to load again.
+      </p>
       <Users />
     </div>
   );
